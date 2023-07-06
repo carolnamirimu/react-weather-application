ae94c68e9417d73a8a4d93e1bb95dad1091e1390
carolnamirimu <105634647+carolnamirimu@users.noreply.github.com>
2023-07-06 15:33:47 +0300

    updated weather.js
    connected axios to different elements enabling live data

--- src/Weather.js
+++ src/Weather.js
@@ -2,26 +2,27 @@ import React, {useState} from "react";
 import axios from 'axios';
 import './Weather.css';
 
- function Weather(){
+ export default function Weather(){
     const[weatherData, setWeatherData]=useState({ready:false});
     function displayWeather(response){
         console.log(response.data);
         setWeatherData({
+            ready:true,
             temperature:response.data.main.temp,
-            hunmidity:response.data.main.humidity,
+            humidity:response.data.main.humidity,
             date:"Wednesday 07:00",
             description: response.data.weather[0].description,
             iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
             wind:response.data.wind.speed,
             city:response.data.name
         });
+
     }
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let city ="Kampala";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    
+    if(weatherData.ready){
     return(
         <div className="Weather mt-5 p-3">
+            
             <form>
                 <div className="row">
                     <div className="col-6">
@@ -32,24 +33,23 @@ import './Weather.css';
                  <button type="button" class="btn btn-primary">Search</button>
                  </div>
                  </div>
+               
             </form>
-<h1>Kampala</h1>
+<h1>{weatherData.city}</h1>
 <div className="row">
     <div className="col-6">
         <ul>
             <li>
                 {weatherData.date}
             </li>
-            <li className="text-capitalise">
+            <li className="text-capitalize">
                 {weatherData.description}
             </li>
         </ul>
     </div>
     <div className="col-6">
         <ul>
-            <li>
-                Presipitation: 15%
-            </li>
+        
             <li>
                 Humidity: {weatherData.humidity} %
             </li>
@@ -60,9 +60,16 @@ import './Weather.css';
     </div>
 </div>
 <div className="">
-    <img src={weatherData.iconUrl} alt={weatherData.description}/> <span className="temp">{weatherData.temperature}</span><span className="units">°C </span>
+    <img src={weatherData.iconUrl} alt={weatherData.description}/> <span className="temp">{Math.round(weatherData.temperature)}</span><span className="units">°C </span>
 </div>
         </div>
-        );
-}
-export default Weather
+                   );}
+                     else{
+                        let apiKey = "62231151ce343c4d68652e1617efc22f";
+                        let city ="Kampala";
+                        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+                        
+                        axios.get(apiUrl).then(displayWeather);
+        
+                        return("Loading....");
+                        }}
