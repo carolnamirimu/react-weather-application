0eac5311ba1af64a5dffbd5084d98ad1a3389853
carolnamirimu <105634647+carolnamirimu@users.noreply.github.com>
2023-07-10 14:52:37 +0300

    included different links to better user experience

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
 <Weather defaultcity="Entebbe"/>
        <footer className='mt-3'>
-        <p> This Application was coded by Namirimu Carol and its
-          <a href='https://github.com/carolnamirimu/react-weather-application'> open source at Github </a>.
+        <p> This App was coded by <a href='https://www.linkedin.com/in/carol-namirimu-50b660127/'target='blank'>Namirimu Carol</a> and its
+          <a href='https://github.com/carolnamirimu/react-weather-application'target='blank'> open source at Github </a> and <a href='https://dashing-cheesecake-d1b3a4.netlify.app/'target='blank'>hosted on netlify</a>.
         </p>
        </footer>
       </header>
