b5a7c17177b97095ed72933796b25ddb9192dd21
carolnamirimu <105634647+carolnamirimu@users.noreply.github.com>
2023-07-12 15:45:32 +0300

    added use effect to reload the forecast on receiving new longtitude and latitude when a new city is intoduced

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import axios from "axios";
 import "./Weatherforecast.css";
 import Weatherforecastday from "./Weatherforecastday";
@@ -7,6 +7,10 @@ export default function WeatherForecast(props){
     let [loaded, setLoaded]=useState(false);
     let [forecast, setForecast]=useState(null);
 
+    useEffect(()=>{
+        setLoaded(false);
+    },[props.coordinates]);
+  
     function displayforecast(response){
       
        setForecast(response.data.daily);
