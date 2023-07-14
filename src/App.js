a1015570ad8b036bcc1cfcf039675b23fe10c9d1
carolnamirimu <105634647+carolnamirimu@users.noreply.github.com>
2023-07-14 13:27:28 +0300

    updated the netlify link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 <Weather defaultcity="london"/>
        <footer className='mt-3'>
         <p> This App was coded by <a href='https://www.linkedin.com/in/carol-namirimu-50b660127/'target='blank'>Namirimu Carol</a> and its
-          <a href='https://github.com/carolnamirimu/react-weather-application'target='blank'> open source at Github </a> and <a href='https://eclectic-dragon-0a6d0f.netlify.app/'target='blank'>hosted on netlify</a>.
+          <a href='https://github.com/carolnamirimu/react-weather-application'target='blank'> open source at Github </a> and <a href='https://glittering-phoenix-d587dd.netlify.app/'target='blank'>hosted on netlify</a>.
         </p>
        </footer>
       </header>
